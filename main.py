25cee57279b1d5d85afde6943264314922bb3176
Shahin Parveen <shahinp2512@gmail.com>
2025-05-19 22:48:16 +0530

    Updated cors

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ app = FastAPI()
 # CORS configuration - use FastAPI's middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
